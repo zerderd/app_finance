2895d7a88531807bec06750e0e29a2a7ba580184
Bezzagame <102792810+Bezzagame@users.noreply.github.com>
2023-12-16 16:39:38 +0700

    авторизация и проверка

--- temp2.py
+++ temp2.py
@@ -11,8 +11,30 @@
 # password = input('Введите пароль: ')
 # file.write(login +':'+password)
 # file.close()
-with open('user.txt', 'a', encoding='utf-8') as file:
+# with open('user.txt', 'a', encoding='utf-8') as file:
+#     login = input('Введите логин: ')
+#     password = input('Введите пароль: ')
+#     file.write(login +':'+password+'\n')
+
+def auth():
     login = input('Введите логин: ')
     password = input('Введите пароль: ')
-    file.write(login +':'+password+'\n')
+    with open('users.txt', 'r', encoding='utf-8') as file:
+        for i in file.readlines():
+            reg_login, reg_password = i.strip().split(':')
+            if reg_login == login and reg_password == password:
+                print('Доступ разрешен')
+                return
+        print('Логин или пароль неверны. Перейдите к регистрации.')
+auth()
 
+
+def check():
+    lst = []
+    with open('users.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            login, password = line.strip().split(":")
+            lst.append((login, password))
+    new_list = [item for sublist in lst for item in sublist]
+    print(new_list)
+check()
